b3e9790208fe78c61c586d33daa0df620207f52a
Deepak <chaubeydeepak903@gmail.com>
2024-03-23 09:13:23 +0530

    updating changes

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,9 @@ import Commom from "./Commom";
 const About = () => {
     return (
         <>
+            <section className="About_section">
             <Commom title="MERN Stack Developer" name="" imgsrc={web} visit="/contact" btname="Contact Now" content="Have a project in mind, or just want to say hello and I'll get back to you as soon as possible. Looking forward to connecting with you!" />
+            </section>
         </>
     )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,9 @@ import Commom from "./Commom";
 const Home = () => {
     return (
         <>
-           <Commom title="Grow Your business with " imgsrc={web} visit="/services" name="Deepak chaubey" btname=" Get Stated" content="We are the Team of Talentes developer Making websites" />
+            <section className="home_section">
+            <Commom title="Grow Your business with " imgsrc={web} visit="/services" name="Deepak chaubey" btname=" Get Stated" content="We are the Team of Talentes developer Making websites" />
+            </section>
         </>
     )
 }
